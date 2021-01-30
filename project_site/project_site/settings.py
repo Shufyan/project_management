e5abe70460334da45d141c10af5b358252d65558
Shufyan <skhan.lotus@gmail.com>
2021-01-30 14:42:35 +0530

    project_management v0.1 - Modified "settings.py"

--- project_site/project_site/settings.py
+++ project_site/project_site/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%)@r#4x-gw%lm5jh&2xflm3emf(#3q8qm#_r&nu80+a83&a1p7'
+SECRET_KEY = '<Add Seceret Key>'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
