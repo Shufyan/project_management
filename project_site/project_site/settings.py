39334ffd0bfe360d203801835cd4e568765cf2f6
Shufyan <skhan.lotus@gmail.com>
2021-02-01 02:59:03 +0530

    Modified - index.html

--- project_site/project_site/settings.py
+++ project_site/project_site/settings.py
@@ -23,7 +23,7 @@ STATIC_DIR = os.path.join(BASE_DIR,'static')
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '<Add Seceret Key>'
+SECRET_KEY = '<Add Secret Key>'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
